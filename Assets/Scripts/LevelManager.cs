eee3f494c5ca76ae4cdeac69aba9e9cbcab1c883
mikeohl <mikeohl@live.com>
2018-03-01 20:37:14 -0800

    Updated depreciated application.LoadLevel to SceneManager.LoadLevel

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿/* LevelManager is responsible for loading the 
+ * next scene and quiting the application.
+ * Uses Unity Scene Management for loading scenes. 
+ */
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel (string name) {
+		// Debug.Log ("Level load requested for: " + name);
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
+	}
+	
+	public void QuitRequest () {
+		// Debug.Log ("Player wants to quit");
+		Application.Quit();
+	}
+
+}
